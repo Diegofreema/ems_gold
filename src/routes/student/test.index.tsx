020cdd77f8641c90c1d738f81b72af77055ff972
Diego <diegofreeman78@gmail.com>
2026-08-31 14:52:49 +0100

    feat: remove obsolete test and receipt components; add new test page and related functionalities

--- src/routes/student/test.index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { TestPage } from '@/portals/student/features/test/test-page'
-
-export const Route = createFileRoute('/student/test/')({
-  staticData: { title: 'Take a test', crumb: 'Assessment' },
-  component: TestPage,
-})
